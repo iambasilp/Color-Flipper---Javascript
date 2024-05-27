e86b36f32c380a049789ceacd6bf94e01e31d07f
iambasilp <iambasilp@gmail.com>
2024-05-27 15:07:56 +0530

    applied some cool logic

--- main.js
+++ main.js
@@ -2,26 +2,21 @@ const button = document.querySelector(".button");
 const colorText = document.querySelector("span");
 console.log(colorText)
 
-function getRandomHexColor() {
-    // Generate random values for red, green, and blue components
-    var red = Math.floor(Math.random() * 256); // Random number between 0 and 255
-    var green = Math.floor(Math.random() * 256);
-    var blue = Math.floor(Math.random() * 256);
+const array = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 
-    // Convert decimal to hexadecimal
-    var redHex = red.toString(16).padStart(2, '0'); // Ensure two digits
-    var greenHex = green.toString(16).padStart(2, '0');
-    var blueHex = blue.toString(16).padStart(2, '0');
-
-    // Concatenate the components to form the color string
-    var hexColor = '#' + redHex + greenHex + blueHex;
-
-    return hexColor;
+function getRandomHexColor(length) {
+   let hex = "#";
+   for(let i = 0 ; i < length; i++){
+    const RandomIndex = Math.floor (Math.random() * array.length)
+    hex += array[RandomIndex]
+   }
+   return hex
 }
 
 button.addEventListener("click", (e) => {
     e.preventDefault()
-    var RandomColor = getRandomHexColor();
+    var RandomColor = getRandomHexColor(6);
+    console.log(RandomColor)
     document.body.style.backgroundColor = `${RandomColor}`;
-    colorText.textContent = `${RandomColor}`
+    colorText.innerText = `${RandomColor}`
 });
