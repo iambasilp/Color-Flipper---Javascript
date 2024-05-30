19f802a09484150acfe09f339f7828137feaf5bb
iambasilp <iambasilp@gmail.com>
2024-05-30 10:27:11 +0530

    updated

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const button = document.querySelector(".button");
 const colorText = document.querySelector("span");
-console.log(colorText)
 
 const array = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 
@@ -13,10 +12,10 @@ function getRandomHexColor(length) {
    return hex
 }
 
-button.addEventListener("click", (e) => {
+button.addEventListener("click", function(e){
+    this.style.border = "thick solid #0000FF"
     e.preventDefault()
     var RandomColor = getRandomHexColor(6);
-    console.log(RandomColor)
     document.body.style.backgroundColor = `${RandomColor}`;
     colorText.innerText = `${RandomColor}`
 });
@@ -36,3 +35,4 @@ button.addEventListener("click", (e) => {
     // allows to create funciton expression 
     // this keyword not bound to the function
     // IIFE
+// how javascript works
